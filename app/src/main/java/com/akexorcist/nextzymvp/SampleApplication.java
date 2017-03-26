9dc74fa56416533bc0c77fd3b9f5180f9f00635d
Akexorcist <akexorcist@gmail.com>
2017-03-27 00:00:21 +0700

    [+] Update cookie jar initialization

--- app/src/main/java/com/akexorcist/nextzymvp/SampleApplication.java
+++ app/src/main/java/com/akexorcist/nextzymvp/SampleApplication.java
@@ -12,6 +12,6 @@ public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        NextworkWebkitCookieJar.getInstance().initCompat(getApplicationContext());
+        NextworkWebkitCookieJar.getInstance().init(getApplicationContext());
     }
 }
